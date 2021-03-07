db3ba15555ac71cae1c5a245138fff09ccb49e53
Viktor Šeňo <viktors5896@gmail.com>
2021-03-07 19:42:15 +0100

    7. RestAPI / SpringBoot

--- src/main/java/program/model/Program.java
+++ src/main/java/program/model/Program.java
@@ -3,7 +3,7 @@ import yajco.annotation.Range;
 
 /*Hlavna trieda reprezentujuca cely konfiguracny dokument
 Zoznam vsetkych prikazov svetiel, ktore sa postupne vykonavaju, tak ako boli zapisane
-Main class -->> package-info.java
+Api class -->> package-info.java
  */
 
 public class Program {
--- src/main/java/program/model/condition/Condition.java
+++ src/main/java/program/model/condition/Condition.java
@@ -8,20 +8,41 @@ import yajco.annotation.Before;
 public class Condition extends Command {
 
     private ExpBool condition;
-    private Command[] commands;
+    private Command[] commands1;
+    private Command[] commands2;
 
     @Before("if")
-    public Condition(ExpBool condition, @Before("{") @After("}") Command[] commands) {
+    public Condition(ExpBool condition, @Before("{") @After("}") Command[] commands1) {
         this.condition = condition;
-        this.commands = commands;
+        this.commands1 = commands1;
+    }
+
+    @Before("if")
+    public Condition(ExpBool condition, @Before("{") @After("}") Command[] commands1,
+                     @Before("else{") @After("}") Command[] commands2) {
+        this.condition = condition;
+        this.commands1 = commands1;
+        this.commands2 = commands2;
     }
 
 
     @Override
     public void execute() {
-        if (condition.evaluateBool()){
-            for(Command cmd : commands){
-                cmd.execute();
+        if (commands2 != null) {
+            if (condition.evaluateBool()) {
+                for (Command cmd : commands1) {
+                    cmd.execute();
+                }
+            }else{
+                for (Command cmd : commands2) {
+                    cmd.execute();
+                }
+            }
+        }else{
+            if (condition.evaluateBool()) {
+                for (Command cmd : commands1) {
+                    cmd.execute();
+                }
             }
         }
     }
