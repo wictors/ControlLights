d7282ebe4e5a8e4487e8416e824e2aa08fb8cf2f
Bc. Viktor Šeňo <viktor.seno@student.tuke.sk>
2021-04-09 22:50:04 +0200

    10. (SpringApi) -Add TOKEN in Api Controller. Check and compare with token from POST request (SetCongifs)

--- SpringApi/src/main/java/rest/Controller.java
+++ SpringApi/src/main/java/rest/Controller.java
@@ -1,6 +1,8 @@
 package rest;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,6 +18,8 @@ import java.nio.file.StandardCopyOption;
 @CrossOrigin
 @RestController
 public class Controller {
+
+    private static final String API_TOKEN = "PasswordApi0000";
 
     @GetMapping("/getConfigs")
     @ResponseBody
@@ -45,16 +49,25 @@ public class Controller {
     }
 
     @PostMapping("/setConfigs")
-    public void setConfigs(@RequestParam("file") MultipartFile configurations) {
-        String fileName = StringUtils.cleanPath(configurations.getOriginalFilename());
-        Path path = Paths.get(fileName);
-        try {
-            Files.copy(configurations.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-            File configs = new File(String.valueOf(path));
-            ConfigurationParser parser = ConfigurationParser.getInstance();
-            parser.unZipConfigurations(configs);
-        } catch (IOException e) {
-            e.printStackTrace();
+    @ResponseBody
+    public ResponseEntity setConfigs(@RequestParam("file") MultipartFile configurations,
+                                     @RequestParam("token") String token) {
+        if (token.equals(API_TOKEN)) {
+            String fileName = StringUtils.cleanPath(configurations.getOriginalFilename());
+            Path path = Paths.get(fileName);
+            try {
+                Files.copy(configurations.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+                File configs = new File(String.valueOf(path));
+                ConfigurationParser parser = ConfigurationParser.getInstance();
+                parser.unZipConfigurations(configs);
+                return new ResponseEntity(HttpStatus.OK);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println(token);
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
         }
+        return new ResponseEntity(HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
